b686e24564a7e4d90a10cf76898e25a365b076de
KC Juntunen <juntunenkc@lan.amstore.com>
2018-11-30 12:06:29 -0500

    I want these to work if I move the folder, of course.

--- GrabProgramTests/SanityCheckerTest.cs
+++ GrabProgramTests/SanityCheckerTest.cs
@@ -6,35 +6,35 @@ namespace GrabProgramTests {
 	public class SanityCheckerTest {
 		[TestMethod]
 		public void TestGood1659File() {
-			SanityChecker sc = new SanityChecker(@"C:\Users\juntunenkc\Source\Repos\GrabProgram\70479A1659.CNC", @"1659");
+			SanityChecker sc = new SanityChecker(@"..\..\..\70479A1659.CNC", @"1659");
 			sc.Check();
 			Assert.IsTrue(sc.OK);
 		}
 
 		[TestMethod]
 		public void TestBad1659File() {
-			SanityChecker sc = new SanityChecker(@"C:\Users\juntunenkc\Source\Repos\GrabProgram\126761B2147.CNC", @"1659");
+			SanityChecker sc = new SanityChecker(@"..\..\..\126761B2147.CNC", @"1659");
 			sc.Check();
 			Assert.IsFalse(sc.OK);
 		}
 
 		[TestMethod]
 		public void TestGood2147File() {
-			SanityChecker sc = new SanityChecker(@"C:\Users\juntunenkc\Source\Repos\GrabProgram\126754B2147.CNC", @"2147");
+			SanityChecker sc = new SanityChecker(@"..\..\..\126754B2147.CNC", @"2147");
 			sc.Check();
 			Assert.IsTrue(sc.OK);
 		}
 
 		[TestMethod]
 		public void TestBad2147File() {
-			SanityChecker sc = new SanityChecker(@"C:\Users\juntunenkc\Source\Repos\GrabProgram\70590A1659.CNC", @"2147");
+			SanityChecker sc = new SanityChecker(@"..\..\..\70590A1659.CNC", @"2147");
 			sc.Check();
 			Assert.IsFalse(sc.OK);
 		}
 
 		[TestMethod]
 		public void TestCouldNotParse() {
-			SanityChecker sc = new SanityChecker(@"C:\Users\juntunenkc\Source\Repos\GrabProgram\test.CNC", @"2147");
+			SanityChecker sc = new SanityChecker(@"..\..\..\test.CNC", @"2147");
 			sc.Check();
 			Assert.IsTrue(sc.OK);
 			Assert.IsFalse(sc.SuccessfullyParsed);
@@ -42,7 +42,7 @@ namespace GrabProgramTests {
 
 		[TestMethod]
 		public void TestEmptyFile() {
-			SanityChecker sc = new SanityChecker(@"C:\Users\juntunenkc\Source\Repos\GrabProgram\test2.CNC", @"2147");
+			SanityChecker sc = new SanityChecker(@"..\..\..\test2.CNC", @"2147");
 			Assert.ThrowsException<System.ArgumentNullException>(new System.Action(sc.Check));
 			Assert.IsFalse(sc.OK);
 			Assert.IsFalse(sc.SuccessfullyParsed);
